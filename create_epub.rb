67282c69b751f91eb12c45dc89b70465e7c96e06
陈刑 <amaliegay@outlook.com>
2024-04-30 17:47:08 +0800

    export epub

--- create_epub.rb
+++ create_epub.rb
@@ -1,32 +1,28 @@
 require 'gepub'
 
-url = 'https://so.gushiwen.cn/guwen/book_46653FD803893E4FECEF70A63E964959.aspx'
-title = '女仙外史'
-author = '吕熊'
-contributors = ['古诗文网', '陈刑']
-body
+def generate_epub(url, title, author, contributors, body)
+  gbook = GEPUB::Book.new do |book|
+    book.identifier = url
+    book.title = title
+    book.creator = author
+    contributors.each do |contributor|
+      book.add_contributor contributor
+    end
+    book.language = 'zh'
 
-gbook = GEPUB::Book.new do |book|
-  book.identifier = url
-  book.title = title
-  book.creator = author
-  contributors.each do |contributor|
-    book.add_contributor contributor
+    book.ordered do
+      item = book.add_item(title + '.xhtml')
+      item.add_content StringIO.new('
+        <html xmlns="http://www.w3.org/1999/xhtml">
+          <head>
+            <title>%s</title>
+          </head>
+          <body>
+            %s
+          </body>
+        </html>' % [title, body.join('')]
+      )end
   end
-  book.language = 'zh'
 
-  book.ordered do
-    item = book.add_item(title + '.xhtml')
-    item.add_content StringIO.new('
-      <html xmlns="http://www.w3.org/1999/xhtml">
-        <head>
-          <title>%s</title>
-        </head>
-        <body>
-          %s
-        </body>
-      </html>' % [title, body]
-    )end
+  gbook.generate_epub(title + ".epub")
 end
-
-gbook.generate_epub(title + ".epub")
--- scrape.rb
+++ scrape.rb
@@ -1,6 +1,6 @@
 require 'httparty'
 require 'nokogiri'
-require 'csv'
+load 'create_epub.rb'
 
 def scrape_chapter(url)
   response = HTTParty.get(url)
@@ -16,33 +16,30 @@ def scrape_chapter(url)
   end
 end
 
-def scrape(book_url, base_url, title)
+def scrape(book_url, base_url, title, author, contributors)
   response = HTTParty.get(book_url)
   if response.code == 200
-    CSV.open(
-      title + '.csv',
-      'w+',
-      write_headers: false,
-      headers: %w[Title, Body]
-    ) do |csv|
-      document = Nokogiri::HTML4(response.body)
+    document = Nokogiri::HTML4(response.body)
 
-      book_container = document.css('.bookcont')
-      chapters_to_scrape = book_container.css('span a')
-      chapters_to_scrape.each do |chapter_a|
-        chapter_title = chapter_a.text
-        if chapter_title != "跋"
-          url = base_url + chapter_a.attribute("href")
-          puts '下载 ' + chapter_title + ' 中 (' + url + ')'
-          chapter = scrape_chapter(url)
-          csv << chapter
-        end
+    book_container = document.css('.bookcont')
+    chapters_to_scrape = book_container.css('span a')
+    body = []
+    chapters_to_scrape.each do |chapter_a|
+      chapter_title = chapter_a.text
+      if chapter_title != "跋"
+        url = base_url + chapter_a.attribute("href")
+        puts '下载 ' + chapter_title + ' 中 (' + url + ')'
+        chapter = scrape_chapter(url)
+        body.push(chapter)
       end
     end
+    generate_epub(book_url, title, author, contributors, body)
   end
 end
 
 book_url = 'https://so.gushiwen.cn/guwen/book_46653FD803893E4FECEF70A63E964959.aspx'
 base_url = 'https://so.gushiwen.cn'
 book_title = '女仙外史'
-scrape(book_url, base_url, book_title)
+author = '吕熊'
+contributors = ['古诗文网', '陈刑']
+scrape(book_url, base_url, book_title, author, contributors)
