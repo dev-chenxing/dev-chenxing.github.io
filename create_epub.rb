37939c4ca35a5d1bac1459d81e66879dc356f127
陈刑 <amaliegay@outlook.com>
2024-05-04 12:00:07 +0800

    booklist.csv

--- create_epub.rb
+++ create_epub.rb
@@ -2,19 +2,7 @@
 
 require 'gepub'
 
-def get_content(title, body)
-  "
-  <html xmlns='http://www.w3.org/1999/xhtml'>
-    <head>
-      <title>#{title}</title>
-    </head>
-    <body>
-      #{body.join('<br>')}
-    </body>
-  </html>"
-end
-
-def generate_epub(url, title, author, contributors, body)
+def generate_epub(url, title, author, contributors, _)
   book = GEPUB::Book.new
   book.identifier = url
   book.title = title
@@ -26,12 +14,12 @@ def generate_epub(url, title, author, contributors, body)
     book.add_item('Styles/style.css', content: css)
   end
 
-  File.open 'cover.jpg', mode: 'rb' do |jpg|
-    book.add_item('Images/cover.jpg', content: jpg).cover_image
+  File.open 'cover.png', mode: 'rb' do |png|
+    book.add_item('Images/cover.png', content: png).cover_image
   end
 
   book.ordered do
-    book.add_item('Text/cover.xhtml', content: StringIO.new(<<~COVER.chomp)).landmark(type: 'cover', title: '封面') }
+    book.add_item('Text/cover.xhtml', content: StringIO.new(<<~COVER.chomp)).landmark(type: 'cover', title: '封面')
       <?xml version="1.0" encoding="utf-8"?>
       <!DOCTYPE html>
       <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
@@ -40,10 +28,13 @@ def generate_epub(url, title, author, contributors, body)
           <title>封面</title>
         </head>
         <body>
-          <div class="cover"><img alt="cover" src="../Images/cover.jpg"/></div>
+          <div class="cover"><img alt="cover" src="../Images/cover.png"/></div>
         </body>
       </html>
     COVER
+  end
 
-  book.generate_epub("#{title}.epub")
+  file = "#{title}.epub"
+  book.generate_epub(file)
+  puts "#{title}.epub created"
 end
--- scrape.rb
+++ scrape.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'csv'
 require 'httparty'
 require 'nokogiri'
 load 'create_epub.rb'
@@ -26,21 +27,47 @@ def parse_chapter_a(chapter_a, base_url)
   scrape_chapter(url)
 end
 
-def scrape(book_url, base_url, title, author, contributors)
+def create_csv(book_url, title, base_url)
   response = HTTParty.get(book_url)
   return unless response.code == 200
 
   body = []
-  Nokogiri::HTML4(response.body).css('span a').each do |chapter_a|
-    chapter = parse_chapter_a(chapter_a, base_url)
-    body.push(chapter) if chapter
+  CSV.open("#{title}.csv", 'w+', write_headers: false, headers: %w[Title Body]) do |csv|
+    Nokogiri::HTML4(response.body).css('span a').each do |chapter_a|
+      chapter = parse_chapter_a(chapter_a, base_url)
+      if chapter
+        body.push(chapter)
+        csv << chapter
+      end
+    end
   end
+  body
+end
+
+def read_csv(title)
+  body = []
+  CSV.foreach(File.expand_path("#{title}.csv", __dir__)) do |chapter|
+    body.push(chapter)
+  end
+  body
+end
+
+def scrape(book_url, base_url, title, author, contributors)
+  body = if File.exist?("#{title}.csv")
+           read_csv(title)
+         else
+           create_csv(book_url, title, base_url)
+         end
   generate_epub(book_url, title, author, contributors, body)
 end
 
-book_url = 'https://so.gushiwen.cn/guwen/book_46653FD803893E4FECEF70A63E964959.aspx'
 base_url = 'https://so.gushiwen.cn'
-book_title = '女仙外史'
-author = '吕熊'
 contributors = %w[古诗文网 陈刑]
-scrape(book_url, base_url, book_title, author, contributors)
+
+booklist = File.expand_path('booklist.csv', __dir__)
+CSV.read(booklist)[1..].each do |book_data|
+  book_url = book_data[0]
+  book_title = book_data[1]
+  author = book_data[2]
+  scrape(book_url, base_url, book_title, author, contributors)
+end
